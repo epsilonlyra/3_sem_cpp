083e3a4abd2a02affa324e541fe85c5fc91cefbe
epsilonlyrae <kozlov.de@phystech.edu>
2022-09-04 12:38:21 +0300

    made D

--- classwork01/D.cpp
+++ classwork01/D.cpp
@@ -1,15 +1,26 @@
 #include<iostream>
-#include<cstring>
 #include<limits>
 
-void describe(void* type_exampler){
-    sizeof(reinterpret_cast<)
-
-
-}
 
 int main(){
 
+std :: cout << "char" << ' '<<  8 * sizeof(char) << ' ' <<
+    int(std::numeric_limits<char>::min()) << ' ' <<  int(std::numeric_limits<char>::max()) << '\n';
+
+std :: cout << "short int" << ' '<<  8 * sizeof(short int) << ' ' <<
+    std::numeric_limits<short int>::min() << ' ' <<  std::numeric_limits<short int>::max() << '\n';
+
+std :: cout << "int" << ' '<<  8 * sizeof(int) << ' ' <<
+    std::numeric_limits<int>::min() << ' ' <<  std::numeric_limits<int>::max() << '\n';
+
+std :: cout << "long int" << ' '<<  8 * sizeof(long int) << ' ' <<
+    std::numeric_limits<long int>::min() << ' ' <<  std::numeric_limits<long int>::max() << '\n';
+
+std :: cout << "float" << ' '<<  8 * sizeof(float) << ' ' <<
+    std::numeric_limits<float>::min() << ' ' <<  std::numeric_limits<float>::max() << '\n';
+
+std :: cout << "double" << ' '<<  8 * sizeof(double) << ' ' <<
+    std::numeric_limits<double>::min() << ' ' <<  std::numeric_limits<double>::max() << '\n';
 
 return 0;
 }

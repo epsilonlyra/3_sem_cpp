d74fb99e7c7e59e175acf7f125cb0656e31d3bc0
epsilonlyra <kozlov.de@phystech.edu>
2022-10-15 20:54:24 +0300

    sus behaviour

--- classwork06/smart_ptrs.cpp
+++ classwork06/smart_ptrs.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 
 template <typename T>
+
 class unique_ptr {
 
     private:
@@ -10,8 +11,8 @@ class unique_ptr {
 
         explicit unique_ptr(T* ptr) : ptr(ptr) {}
 
-        unique_ptr (unique_ptr&& other) : ptr(ptr.other) {
-            other.ptr =nullptr;
+        unique_ptr (unique_ptr&& other) : ptr(other.ptr) {
+            other.ptr = nullptr;
         }
 
         unique_ptr& operator=(unique_ptr&& other) {
@@ -39,36 +40,43 @@ class unique_ptr {
 template <typename T>
 
 class shared_ptr {
+
     public:
-        shared_ptr(T* ptr) {
-            cptr = new ContolBlock;
-            cptr->counter = 1;
-            cptr->object = ptr;
+        explicit shared_ptr(T* ptr) {
+            control_pointer = new ControlBlock<T*>;
+            control_pointer->counter = 1;
+            control_pointer->object = ptr;
         }
 
-        shared_ptr(const T&other) : {
-            other.cptr->counter++;
-            cptr = other.cptr;
+        shared_ptr (const shared_ptr& other) : control_pointer(other.control_pointer) {
+            control_pointer->counter++;
         }
 
-        shared_ptr(T&& other) : cptr(other.cptr) {
-            other.cptr = nullptr;
+        shared_ptr (shared_ptr&& other) : control_pointer(other.control_pointer) {
+                other.control_pointer = nullptr;
         }
+
+        shared_ptr& operator= (shared_ptr&& other) = delete;
+
+        shared_ptr& operator= (const shared_ptr& other) = delete;
 
         size_t use_count() const {
-            return cptr->counter;
+            return control_pointer->counter;
         }
 
-        ~shared_ptr(){
-            cptr->counter--;
-
-            if (cptr->counter == 0) {
-                delete cptr->object;
-                delete cptr->counter;
+        ~shared_ptr() {
+            control_pointer->counter--;
+            if (control_pointer->counter == 0) {
+                delete control_pointer;
             }
+        }
 
-            delete cptr;
+        T& operator* ()  const {
+            return *(control_pointer->object);
+        }
 
+        T* operator-> () const {
+            return control_pointer->object;
         }
 
     private:
@@ -77,20 +85,40 @@ class shared_ptr {
         struct ControlBlock {
             size_t counter = 0;
             U object = nullptr;
+
+            ~ControlBlock() {
+                delete object;
+            }
         };
 
-        ControlBlock* cptr = nullptr;
+        ControlBlock<T*>* control_pointer = nullptr;
 
         template <typename U, typename... Args>
         friend shared_ptr<U> make_shared(Args&&... args);
 };
 
+template <typename T, typename... Args>
+
+    shared_ptr<T> make_shared(Args&&... args) {
+      auto ptr = new T(std::forward<Args>(args)...);
+      return shared_ptr<T>(ptr);
+    }
+
+
 
 int main() {
 
 
+shared_ptr<int> A(new int);
 
 
+shared_ptr<int> K  = make_shared<int>(1);
+
+
+//shared_ptr<int> B(A);
+
+
+//std :: cout << A.use_count();
 return 0;
 
 }
